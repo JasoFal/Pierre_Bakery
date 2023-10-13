05447758ff717bd70aa405146f579380cc965590
Jason Falk <jasonfalk9@gmail.com>
2023-10-13 13:39:33 -0700

    Add Bread init object functionality + test.

---
+++ PierreBakery.Tests/ModelTests/Bread.Test.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierreBakery.Models;
+
+namespace PierreBakery.Tests
+{
+  [TestClass]
+  public class BreadTests
+  {
+    [TestMethod]
+    public void BreadConstructor_CreatesInstanceOfBread_Bread()
+    {
+      Bread newBread = new Bread();
+      Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+  }
+}
---
+++ PierreBakery/Models/Bread.cs
@@ -0,0 +1,10 @@
+namespace PierreBakery.Models
+{
+  public class Bread
+  {
+    public Bread()
+    {
+      
+    }
+  }
+}
