952f45fb99d1c0de81fb14bf91c2de4322654448
Jason Falk <jasonfalk9@gmail.com>
2023-10-17 14:07:22 -0700

    Add UI response for Bread and Pastry.

---
+++ PierreBakery/Program.cs
@@ -0,0 +1,151 @@
+using System;
+using System.Runtime.CompilerServices;
+using PierreBakery.Models;
+
+namespace PierreBakery
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      Console.WriteLine("Welcome to Pierre Bakery.");
+      Console.WriteLine("On our menu we have two options.");
+      Console.WriteLine("Bread costs $5 each.");
+      Console.WriteLine("Pastries cost $2 each.");
+      Console.WriteLine("If you wish to purchase bread type in 'bread' below, if you wish to purchase pastries type in pastry below.");
+      string userResponse = Console.ReadLine();
+      if (userResponse.ToLower() == "bread")
+      {
+        PurchaseBreadUIResponse();
+      }
+      else if (userResponse.ToLower() == "pastry")
+      {
+        PurchasePastryUIResponse();
+      }
+      else
+      {
+        Console.WriteLine("Please enter something we actually sell!");
+        Main();
+      }
+    }
+    static void PurchaseBreadUIResponse()
+    {
+      Console.WriteLine("Please enter how many loafs you desire.");
+      string userInputBread = Console.ReadLine();
+      try
+      {
+        int shoppingCartBread = int.Parse(userInputBread);
+        Bread shopBread = new Bread(shoppingCartBread);
+        ConfirmOrEditShopBread(shopBread);
+      }
+      catch
+      {
+        Console.WriteLine("!!!!!!!!!!!!!!!!");
+        Console.WriteLine("Error: Please only enter in numbers! Special symbols and alphabetic characters will not be accepted.");
+        Console.WriteLine("Please try again...");
+        Main();
+      }
+    }
+    static void PurchasePastryUIResponse()
+    {
+      Console.WriteLine("Please enter how many pastries you desire.");
+      string userInputPastry = Console.ReadLine();
+      try
+      {
+        int shoppingCartPastries = int.Parse(userInputPastry);
+        Pastry shopPastry = new Pastry(shoppingCartPastries);
+        ConfirmOrEditShopPastry(shopPastry);
+      }
+      catch
+      {
+        Console.WriteLine("!!!!!!!!!!!!!!!!");
+        Console.WriteLine("Error: Please only enter in numbers! Special symbols and alphabetic characters will not be accepted.");
+        Console.WriteLine("Please try again...");
+        Main();
+      }
+    }
+    static void ConfirmOrEditShopBread(Bread shopBread)
+    {
+      Console.WriteLine("Please confirm purchase.");
+      Console.WriteLine("Shopping Cart:");
+      Console.WriteLine("-----------------------");
+      Console.WriteLine($"Bread: {shopBread.AmountOfBread}");
+      Console.WriteLine("-----------------------");
+      Console.WriteLine("Is this correct? If so enter 'yes' below, if not enter any other key.");
+      string correctAmountOfBread = Console.ReadLine();
+      if (correctAmountOfBread.ToLower() == "yes")
+      {
+        CalculateShoppingCartBread(shopBread);
+      }
+      else
+      {
+        Console.WriteLine("Please reenter the amount of loafs desired:");
+        string reenteredShopCartBread = Console.ReadLine();
+        shopBread.AmountOfBread = int.Parse(reenteredShopCartBread);
+        ConfirmOrEditShopBread(shopBread);
+      }
+    }
+    static void ConfirmOrEditShopPastry(Pastry shopPastry)
+    {
+      Console.WriteLine("Please confirm purchase");
+      Console.WriteLine("Shopping Cart:");
+      Console.WriteLine("-----------------------");
+      Console.WriteLine($"Pastry: {shopPastry.AmountOfPastry}");
+      Console.WriteLine("-----------------------");
+      Console.WriteLine("Is this correct? If so enter 'yes' below, if not enter any other key.");
+      string correctAmountOfPastry = Console.ReadLine();
+      if (correctAmountOfPastry.ToLower() == "yes")
+      {
+        CalculateShoppingCartPastry(shopPastry);
+      }
+      else
+      {
+        Console.WriteLine("Please reenter the amount of pastries desired:");
+        string reenteredShopCartPastry = Console.ReadLine();
+        shopPastry.AmountOfPastry = int.Parse(reenteredShopCartPastry);
+        ConfirmOrEditShopPastry(shopPastry);
+      }
+    }
+    static void CalculateShoppingCartBread(Bread shopBread)
+    {
+      shopBread.CalculateTotalBreadPrice();
+      Console.WriteLine("----------------------------------------------------------");
+      Console.WriteLine("Your total is:");
+      Console.WriteLine($"${shopBread.TotalBreadCost}");
+      Console.WriteLine("----------------------------------------------------------");
+      Console.WriteLine("Thank you for shopping with us today!");
+      Console.WriteLine("Would you like to try some of our pastries as well?");
+      Console.WriteLine("If so enter 'pastries' below other wise enter any key to exit.");
+      string newOrder = Console.ReadLine();
+      if (newOrder.ToLower() == "pastries")
+      {
+        PurchasePastryUIResponse();
+      }
+      else
+      {
+        Console.WriteLine("Goodbye!");
+      }
+    }
+    static void CalculateShoppingCartPastry(Pastry shopPastry)
+    {
+      shopPastry.CalculateTotalPastryPrice();
+      Console.WriteLine("----------------------------------------------------------");
+      Console.WriteLine("Your total is:");
+      Console.WriteLine($"${shopPastry.TotalPastryCost}");
+      Console.WriteLine("----------------------------------------------------------");
+      Console.WriteLine("Thank you for shopping with us today!");
+      Console.WriteLine("Would you like to try some of our bread as well?");
+      Console.WriteLine("If so enter 'bread' below other wise enter any key to exit.");
+      string newOrder = Console.ReadLine();
+      if (newOrder.ToLower() == "bread")
+      {
+        PurchaseBreadUIResponse();
+      }
+      else
+      {
+        Console.WriteLine("Goodbye!");
+      }
+    }
+  }
+}
